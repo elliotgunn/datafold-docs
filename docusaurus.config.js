d6841da0f1e8fa20e87d2c176dc349994949ef02
Sergei <sergeyklinov@gmail.com>
2023-09-29 20:07:39 +0600

    Fix navbar crash issue

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,9 +67,9 @@ const config = {
           // height: 32,
         },
         items: [
-          {type: 'doc', docId: 'welcome', label: 'Welcome', position: 'left'},
-          {type: 'docSidebar', sidebarId: 'guides', label: 'Guides', position: 'left'},
-          {type: 'docSidebar', sidebarId: 'references', label: 'Reference', position: 'left'},
+          // {type: 'doc', docId: 'welcome', label: 'Welcome', position: 'left'},
+          // {type: 'docSidebar', sidebarId: 'guides', label: 'Guides', position: 'left'},
+          // {type: 'docSidebar', sidebarId: 'references', label: 'Reference', position: 'left'},
           // {type: 'docSidebar', sidebarId: 'api', label: 'APIs', position: 'left'},
           // {to: '/blog', label: 'Blog', position: 'left'}, // remove to turn on blog
           // {
